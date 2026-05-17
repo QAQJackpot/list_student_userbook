0edb3b8757af7ab5fe55acc41b6a9ac7327d3c1f
3580129138@qq.com <3580129138@qq.com>
2026-05-17 15:19:11 +0800

    添加学生信息插入和加载功能，更新VSCode配置文件

--- list_student_id/main.cpp
+++ list_student_id/main.cpp
@@ -34,9 +34,45 @@ public:
 	void AgeRecordFind(struct student* h, int x);     //统计指定年龄的学生人数，并显示全部指定年龄学生信息 
 	void SexRecordFind(struct student* h, string x);     //统计指定性别的学生人数，并显示全部指定性别学生信息 
 	void SaveRecordFile(struct student* h);     //保存学生信息到文件 
-	struct score* LoadRecordFile(struct student* h);   //从文件中加载学生信息 
+	struct student* LoadRecordFile(struct student* h);   //从文件中加载学生信息 
 };
 
+struct student* record::Insertstu(struct student* h)
+{
+	student* p1 = NULL, * p2 = NULL;
+
+	p1 = new student;
+	cout << "请输入学生的学号：";
+	cin >> p1->id;
+	cout << "请输入学生的姓名：";
+	cin >> p1->name;
+	cout << "请输入学生的专业：";
+	cin >> p1->major;
+	cout << "请输入学生的班级：";
+	cin >> p1->classes;
+	cout << "请输入学生的性别：";
+	cin >> p1->sex;
+	cout << "请输入学生的年龄：";
+	cin >> p1->age;
+	p1->next = NULL;
+	if (h == NULL || p1->id < h->id)
+	{
+		p1->next = h;
+		h = p1;
+	}
+	else
+	{
+		p2 = h;
+		while (p2->next != NULL && p2->next->id < p1->id)
+		{
+			p2 = p2->next;
+		}
+		p1->next = p2->next;
+		p2->next = p1;
+	}
+
+	return h;
+}
 
 
 void record::SaveRecordFile(struct student* h)
@@ -61,6 +97,39 @@ void record::SaveRecordFile(struct student* h)
 	return;
 }
 
+struct student* record::LoadRecordFile(struct student* h)
+{
+	ifstream ifile;
+	ifile.open("student.dat", ios::in);
+	student *p,*q=NULL;
+	if (!ifile)
+	{
+		cout << "文件打开错误，无法读取\n";
+		return;
+	}
+	char str[50];
+	ifile.getline(str,50);
+	while (!ifile.eof())
+	{
+		student_num++;
+		p = new student;
+		ifile >> p->id >> p->name >> p->major >> p->classes >> p->sex >> p->age; //
+		p->next = NULL;
+		if (h == NULL)
+		{
+			q = h = p;
+		}
+		else
+		{
+			q ->next = p;
+			q = p;
+		}
+	}
+	ifile.close();
+	return h;
+}
+
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -72,39 +141,3 @@ int main()
 	return 0;
 }
 
-struct student* Insert(struct student* h)
-{
-	student* p1 = NULL, * p2 = NULL;
-
-	p1 = new student;
-	cout << "请输入学生的学号：";
-	cin >> p1->id;
-	cout << "请输入学生的姓名：";
-	cin >> p1->name;
-	cout << "请输入学生的专业：";
-	cin >> p1->major;
-	cout << "请输入学生的班级：";
-	cin >> p1->classes;
-	cout << "请输入学生的性别：";
-	cin >> p1->sex;
-	cout << "请输入学生的年龄：";
-	cin >> p1->age;
-	p1->next = NULL;
-	if (h == NULL || p1->num < h->num)
-	{
-		p1->next = h;
-		h = p1;
-	}
-	else
-	{
-		p2 = h;
-		while (p2->next != NULL && p2->next->num < p1->num)
-		{
-			p2 = p2->next;
-		}
-		p1->next = p2->next;
-		p2->next = p1;
-	}
-
-	return h;
-}
